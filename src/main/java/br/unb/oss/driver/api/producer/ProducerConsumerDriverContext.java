5522da1a469b8b90dce03fdbbe5ae0f9e5d1e2e5
Roman <rrrooommmaaa@mail.ru>
2019-10-07 22:58:05 +0300

    cleanup

--- src/main/java/br/unb/oss/driver/api/producer/ProducerConsumerDriverContext.java
+++ src/main/java/br/unb/oss/driver/api/producer/ProducerConsumerDriverContext.java
@@ -1,18 +1,13 @@
 package br.unb.oss.driver.api.producer;
 
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
-// import com.datastax.oss.driver.api.core.cql.PrepareRequest;
-// import com.datastax.oss.driver.api.core.cql.Statement;
 import com.datastax.oss.driver.api.core.metadata.Node;
 import com.datastax.oss.driver.api.core.metadata.NodeStateListener;
 import com.datastax.oss.driver.api.core.metadata.schema.SchemaChangeListener;
 import com.datastax.oss.driver.api.core.tracker.RequestTracker;
 import com.datastax.oss.driver.api.core.type.codec.TypeCodec;
 import com.datastax.oss.driver.internal.core.context.DefaultDriverContext;
-import com.datastax.oss.driver.internal.core.cql.CqlPrepareAsyncProcessor;
-// import com.datastax.oss.driver.internal.core.cql.CqlPrepareSyncProcessor;
 import com.datastax.oss.driver.internal.core.cql.CqlRequestAsyncProcessor;
-import com.datastax.oss.driver.internal.core.cql.CqlRequestSyncProcessor;
 import com.datastax.oss.driver.internal.core.session.RequestProcessorRegistry;
 import java.util.List;
 import java.util.Map;
@@ -42,25 +37,17 @@ public class ProducerConsumerDriverContext extends DefaultDriverContext {
 
   @Override
   public RequestProcessorRegistry buildRequestProcessorRegistry() {
-    // Register the typical request processors, except instead of the normal async processors,
-    // use GuavaRequestAsyncProcessor to return ListenableFutures in async methods.
 
     CqlRequestAsyncProcessor cqlRequestAsyncProcessor = new CqlRequestAsyncProcessor();
-    CqlPrepareAsyncProcessor cqlPrepareAsyncProcessor = new CqlPrepareAsyncProcessor();
-    CqlRequestSyncProcessor cqlRequestSyncProcessor =
-        new CqlRequestSyncProcessor(cqlRequestAsyncProcessor);
+//    CqlPrepareAsyncProcessor cqlPrepareAsyncProcessor = new CqlPrepareAsyncProcessor();
+//    CqlRequestSyncProcessor cqlRequestSyncProcessor = new CqlRequestSyncProcessor(cqlRequestAsyncProcessor);
 
     return new RequestProcessorRegistry(
         getSessionName(),
 
         cqlRequestAsyncProcessor,
 //        new CqlPrepareSyncProcessor(cqlPrepareAsyncProcessor),
-            
-            
-//        new GuavaRequestAsyncProcessor<>(
-//            cqlRequestAsyncProcessor, Statement.class, GuavaSession.ASYNC),
-//        new GuavaRequestAsyncProcessor<>(
-//            cqlPrepareAsyncProcessor, PrepareRequest.class, GuavaSession.ASYNC_PREPARED),
+
         new CustomRequestProcessor(cqlRequestAsyncProcessor));
   }
 }
