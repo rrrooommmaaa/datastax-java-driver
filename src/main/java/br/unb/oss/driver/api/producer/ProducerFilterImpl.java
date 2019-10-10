53fe359b37eeb34490e66aca240f74abc5fb4c39
Roman <rrrooommmaaa@mail.ru>
2019-10-10 12:24:48 +0300

    botched filter attempt

--- src/main/java/br/unb/oss/driver/api/producer/ProducerFilterImpl.java
+++ src/main/java/br/unb/oss/driver/api/producer/ProducerFilterImpl.java
@@ -1,48 +1,102 @@
 package br.unb.oss.driver.api.producer;
 
+import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Predicate;
 
 public class ProducerFilterImpl<T, U> extends ProducerBase<T> implements Consumer<T>, Producer<T> {
-  private final Predicate<T> filter;
-  private Consumer<? super T> consumer = null;
-  private final Producer<T> producer;
 
-  public ProducerFilterImpl(Producer<T> producer, Predicate<T> filter) {
-    this.producer = producer;
-    this.filter = filter;
-  }
+    private final Predicate<T> filter;
+    private Consumer<? super T> consumer = null;
+    private final ProducerBase<T> parentProducer;
 
-  @Override
-  public void consume(T item) {
-    consumer.consume(item);
-  }
-
-  @Override
-  public void operationComplete() {
-    consumer.operationComplete();
-  }
-
-  @Override
-  public void operationAborted(Throwable error) {
-    consumer.operationAborted(error);
-  }
-
-  @Override
-  public void register(Consumer<? super T> consumer) {
-    if (this.consumer != null) {
-      throw new IllegalStateException("Only one consumer is allowed to be registered.");
+    private final AtomicLong allowed = new AtomicLong(0);
+    private final AtomicInteger wip = new AtomicInteger(0);
+    private volatile boolean cancelFlag = false;
+    
+    private volatile T last = null;
+    
+    private void drain() {
+        if (wip.getAndIncrement() == 0) {
+            do {
+                pump();
+            } while (wip.decrementAndGet() != 0);
+        }
     }
-    this.consumer = consumer;
-    producer.register(this);
-  }
 
-  @Override
-  public void produce(long n) {
-    producer.produce(n);
-  }
+    private void pump() {
+        if (!cancelFlag) {
+        }
+    }
 
-  @Override
-  public void cancel() {
-    producer.cancel();
-  }
+    public ProducerFilterImpl(ProducerBase<T> producer, Predicate<T> filter) {
+        this.parentProducer = producer;
+        this.filter = filter;
+    }
+
+    @Override
+    public void consume(T item) {
+        if (cancelFlag) {
+            return;
+        }
+        if (filter.test(item)) {
+            // last is always null at this point
+            last = item;
+            drain();
+        } else {
+            parentProducer.produce(1);
+        }
+    }
+
+    @Override
+    public void operationComplete() {
+        consumer.operationComplete();
+    }
+
+    @Override
+    public void operationAborted(Throwable error) {
+        consumer.operationAborted(error);
+    }
+
+    @Override
+    public void register(Consumer<? super T> consumer) {
+        if (this.consumer != null) {
+            throw new IllegalStateException("Only one consumer is allowed to be registered.");
+        }
+        this.consumer = consumer;
+        parentProducer.register(this);
+        parentProducer.produce(1); // we want to look ahead;
+    }
+
+    @Override
+    public void produce(long n) {
+        if (this.consumer == null) {
+            throw new IllegalStateException("A consumer should be registered first.");
+        }
+        if (n <= 0) {
+            throw new IllegalArgumentException("You should request more than zero rows.");
+        }
+        allowed.addAndGet(n);
+        getExecutor().execute(this::drain);
+    }
+
+    @Override
+    public void cancel() {
+        if (this.consumer == null) {
+            throw new IllegalStateException("A consumer should be registered first.");
+        }
+        cancelFlag = true;
+        parentProducer.cancel();
+    }
+
+    @Override
+    Executor getExecutor() {
+        return parentProducer.getExecutor();
+    }
+
 }
---
+++ src/test/java/ProducerFilterTest.java
@@ -0,0 +1,31 @@
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.after;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import br.unb.oss.driver.api.producer.Consumer;
+import br.unb.oss.driver.api.producer.Producer;
+import br.unb.oss.driver.api.producer.ProducerImpl;
+import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Predicate;
+import org.junit.Test;
+
+public class ProducerFilterTest extends TestBase {
+  @Test
+  public void should_consume_requested_number_of_rows_from_unlimited_supply() {
+    Consumer<Row> consumer = (Consumer<Row>) mock(Consumer.class);
+
+    CompletionStage<AsyncResultSet> stage = generateChainPage();
+    Producer<Row> parentProducer = new ProducerImpl(stage);
+    Predicate<Row> filter = x -> x.get("ID", Integer.class) == 0;
+    Producer<Row> producer = parentProducer.filter(filter);
+
+    producer.register(consumer);
+    producer.produce(10);
+
+    // verify that consumer.consume was called exactly 10 times
+    verify(consumer, after(2000).times(10)).consume(any());
+  }
+}
